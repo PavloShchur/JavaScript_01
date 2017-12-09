c3d55e1795cd021fc0558947e745f969a95a13f4
Pavlo Shchur <pavloshchur@gmail.com>
2017-12-09 13:31:56 +0200

    first commit

--- function/function.js
+++ function/function.js
@@ -1,7 +1,15 @@
-/**
- * Created by User on 21.06.2017.
- */
-
 function sayHello() {
     document.write("Hello there")
 }
+
+function sum(x, y) {
+    v = x + y;
+    alert(v);
+}
+
+window.onload = function () {
+    var x = 10;
+    var y = 20;
+    var v;
+    sum(100, y);
+};
