51bbd01bf65670d665fced030733512caf15aac4
Pavlo Shchur <pavloshchur@gmail.com>
2018-01-08 15:02:34 +0200

    JavaScript_01

--- js/hello_world_4.js
+++ js/hello_world_4.js
@@ -1,7 +1,68 @@
-/**
- * Created by User on 20.06.2017.
- */
-
 function sayHello() {
     alert("Hello Wrld")
 }
+alert('Hello, this is an alert function');
+function useAlert() {
+    alert('Use Alert')
+}
+// JavaScript is a case sensitive
+var a = 1; //integer
+var A = 2;
+
+// var a = 1, A = 2;
+
+console.log('a = ' + a);
+console.log('A = ' + A);
+var someString = "someString"; // string
+console.log('someString = ' + someString);
+
+var boo = false; //boolean
+console.log('boo = ' + boo);
+
+var myObject = {name: "Jogn", email: "pavloshchur@ukr.net", age: 20};
+var myArray = ["one", "two", 30];
+console.log('myObject = ' + myObject);
+console.log('myArray = ' + myArray);
+
+// appleAB is the number of apples I ate.
+var appleAB = "strings whitespace"; // string
+var $App = 2; // integer
+var _boo = false; //boolean true or false
+var myObject = {name: "John", email: "my@email.com", age: 20}; //object
+var myArray = ["one", "two", 3];
+console.log(myObject);
+console.log(myArray);
+//expression function
+var myFunc = function(){console.log('in experssionFunction ' + myObject);};
+//declaration function
+function myFunction() {
+    console.log('in myFunction ' + myObject);
+}
+myFunc();
+myFunction(); //invoke an function
+
+
+/*
+ everything in
+ here is ignored
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
