62d791001a68a39eb5e4222ad2b011d05a5465ae
Pavlo Shchur <pavloshchur@gmail.com>
2018-01-10 23:30:08 +0200

    JavaScript_01

--- function/function.js
+++ function/function.js
@@ -13,3 +13,12 @@ window.onload = function () {
     var v;
     sum(100, y);
 };
+
+function makeRequest(url, timeout, callback) {
+    console.log(arguments.length);
+    timeout = (typeof timeout !== "undefined") ? timeout : 3000;
+    callback = (typeof callback !== "undefined") ? timeout : function () {
+    };
+}
+
+makeRequest('/foo', 300, function () {});
--- js/hello_world_4.js
+++ js/hello_world_4.js
@@ -2,45 +2,18 @@ function sayHello() {
     alert("Hello Wrld")
 }
 
-var myObject = {name: "Pavlo", surname: "Shchur", phone: "12345"};
-console.log(myObject.name);
-console.log(myObject['surname']);
-
-function myFunction(a) {
-    a++;
-    console.log('a = ' + a);
+function gerValue(condition) {
+    const person = {name : 'Pavlo'};
+    if (condition == true) {
+        person.name = 'Shchur Pavlo';
+        console.log(person);
+        return 'TRUE';
+    } else {
+        return 'FALSE';
+    }
 }
 
-for (i = 0; i < 3; i++) {
-    myFunction(i);
-}
-
-var tempVariable;
-for (tempVariable in myObject) {
-    console.log('tempVariable = ' + tempVariable + ' ' + myObject[tempVariable]);
-}
-
-var f = 0;
-
-while (f < 3) {
-    console.log('f = ' + f);
-    f++;
-}
-
-var msg = "Hello World";
-console.log(msg.startsWith('Hello'));
-console.log(msg.startsWith('l', 2));
-console.log(msg.endsWith('World'));
-console.log(msg.includes('World'));
-
-console.log('x'.repeat(3))
-
-
-
-
-
-
-
+console.log(gerValue(true));
 
 
 
