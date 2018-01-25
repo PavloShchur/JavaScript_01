5eafa1bcb0ff446e6801f3f287caa874b752b8f3
Pavlo Shchur <pavloshchur@gmail.com>
2018-01-25 23:36:56 +0200

    JavaScript objects.

--- objects/objectsSimple.js
+++ objects/objectsSimple.js
@@ -11,6 +11,50 @@ let func = function () {
     return "material";
 };
 
-let key = "material";
-console.log(box[key]);
-console.log(box[func()]);
+box["size"] = {height : 20, width : 50};
+
+// area is a method
+box.area = function () {
+    return box.size.height * box.size.width;
+}
+
+for(var key in box){
+    // no gerentee order
+    // console.log(key);
+    // console.log(box[key]);
+}
+
+let animal = [];
+
+animal.username = 'Mittens';
+
+animal['tagline'] = 'Pet me!';
+
+let noises = [];
+animal.noises = noises;
+console.log(animal);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
