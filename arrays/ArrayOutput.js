45a9a5750404ff24585d66dc98c25db0e264a017
Pavlo Shchur <pavloshchur@gmail.com>
2018-01-27 00:16:11 +0200

    JavaScript objects.

---
+++ arrays/ArrayOutput.js
@@ -0,0 +1,49 @@
+// let box = [];
+
+// box[0] = true;
+// box[1] = 'moew';
+//
+// box.push({'hello' : 'goodbuy'});
+//
+// let i = 0;
+//
+// console.log(box[i]);
+// console.log(box.pop());
+// console.log(box);
+
+// box['size'] = 9;
+// box['word'] = 'moew';
+//box.push('whoo!');
+
+// console.log('box[0] : ' + box[0]); // undefined
+
+// for(var kye in box){
+//     console.log(kye + ' ' + box[kye]);
+//     console.log(box.kye);
+// }
+
+// for(let i = 0; i < box.length; i++){
+//     console.log(i + ' ' + box[i]);
+// }
+
+let noiseArray = ['purr', 'oink'];
+noiseArray.unshift('hiss');
+noiseArray.push('moew');
+console.log(noiseArray.length);
+console.log(noiseArray);
+noiseArray[3] = 'growl';
+console.log(noiseArray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- objects/objectsSimple.js
+++ objects/objectsSimple.js
@@ -32,7 +32,19 @@ animal['tagline'] = 'Pet me!';
 
 let noises = [];
 animal.noises = noises;
+
+let count = 0;
+for(let key in animal){
+    count++;
+    if(key === 'username'){
+        console.log('Hi my name is ' + animal[key]);
+    } else if(key === 'tagline'){
+        console.log('I like to say ' + animal[key]);
+    }
+}
+
 console.log(animal);
+
 
 
 
