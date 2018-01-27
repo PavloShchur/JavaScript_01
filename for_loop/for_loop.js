f0846a1395c75b893d9726f2a1a3585273081201
Pavlo Shchur <pavloshchur@gmail.com>
2018-01-27 21:51:28 +0200

    JavaScript objects.

--- for_loop/for_loop.js
+++ for_loop/for_loop.js
@@ -1,12 +1,100 @@
-/**
- * Created by User on 21.06.2017.
- */
+// var count = 0;
+// document.write("Starting Loop" + "<br />");
+// for (count = 0; count < 10; count++){
+//     document.write("Current Count: " + count);
+//     document.write("<br />");
+// }
+//
+// document.write("Loop Stopped");
 
-var count = 0;
-document.write("Starting Loop" + "<br />");
-for (count = 0; count < 10; count++){
-    document.write("Current Count: " + count);
-    document.write("<br />");
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+/*
+0, true, print 0, update i to 1
+1, true, print 1, update i to 2
+.
+.
+.
+9, true, print 9, update i to 10
+10, FALSE, end loop!
+*/
+
+// let names = ['John', 'Jane', 'Mary', 'Mark', 'Bob'];
+
+// for loops
+// for (let i = 0; i < names.length; i++) {
+//     console.log(names[i]);
+// }
+//
+// for (let i = names.length - 1; i >= 0; i--) {
+//     console.log(names[i]);
+// }
+
+// while loops
+// let i = 0;
+// while (i < names.length) {
+//     console.log(names[i]);
+//     i++;
+// }
+
+// break and continue
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i);
+//
+//     if (i === 3) {
+//         break;
+//     }
+// }
+//
+// for (let i = 1; i <= 5; i++) {
+//     if (i === 3) {
+//         continue;
+//     }
+//
+//     console.log(i);
+// }
+
+/*
+1. Create an array with some years where persons were born
+2. Create an empty array (just [] )
+3. Use a loop to fill the array with the ages of the persons
+4. Use another loop to log into the console whether each person is of full age (18 or older), as well as their age
+
+5. Finally, create a function called printFullAge which receives the array of years as an argument, executes the steps 2., 3. and 4. and returns an array of true/false boolean values: true if the person is of full age (>= 18 years) and false if not (< 18 years)
+6. Call the function with two different arrays and store the results in two variables: full_1 and full_2
+
+Example input:  [1965, 2008, 1992]
+Example output: [true, false, true]
+
+Hint: you can use a loop not only to read from an array, like y[i], but also to set values in an array, like y[i] = ... You can also use the specific array methods.
+*/
+
+function printFullAge(years) {
+    let ages = [];
+    let fullAges = [];
+
+    for (let i = 0; i < years.length; i++) {
+        ages[i] = 2016 - years[i];
+    }
+
+    for (i = 0; i < ages.length; i++) {
+        if (ages[i] >= 18) {
+            console.log('Person ' + (i + 1) + ' is ' + ages[i] + ' years old, and is of full age.');
+            fullAges.push(true);
+        } else {
+            console.log('Person ' + (i + 1) + ' is ' + ages[i] + ' years old, and is NOT of full age.');
+            fullAges.push(false);
+        }
+    }
+
+    return fullAges;
 }
 
-document.write("Loop Stopped")
+let years = [2001, 1985, 1994, 2014, 1973];
+let full_1 = printFullAge(years);
+let full_2 = printFullAge([2012, 1915, 1999]);
+
+console.log(full_1);
+console.log(full_2);
--- objects/objectsSimple.js
+++ objects/objectsSimple.js
@@ -1,51 +1,127 @@
-let box = {};
+// let box = {};
+//
+// box.material = "cardboard";
+//
+// let writeToVariable = box.material;
+//
+// box.material = "newValue";
+//
+// box["material"] = "secondValue";
+// let func = function () {
+//     return "material";
+// };
+//
+// box["size"] = {height : 20, width : 50};
+//
+// // area is a method
+// box.area = function () {
+//     return box.size.height * box.size.width;
+// }
+//
+// for(var key in box){
+//     // no gerentee order
+//     // console.log(key);
+//     // console.log(box[key]);
+// }
+//
+// let animal = [];
+//
+// animal.username = 'Mittens';
+//
+// animal['tagline'] = 'Pet me!';
+//
+// let noises = [];
+// animal.noises = noises;
+//
+// let count = 0;
+// for(let key in animal){
+//     count++;
+//     if(key === 'username'){
+//         console.log('Hi my name is ' + animal[key]);
+//     } else if(key === 'tagline'){
+//         console.log('I like to say ' + animal[key]);
+//     }
+// }
+//
+// console.log(animal);
 
-box.material = "cardboard";
+// let john = {
+//     name: 'John',
+//     lastName: 'Smith',
+//     yearOfBirth: 1990,
+//     job: 'teacher',
+//     isMarried: false
+// };
+//
+// console.log(john.lastName);
+// console.log(john['lastName']);
+//
+// let xyz = 'job';
+// console.log(john[xyz]);
+//
+// john.lastName = 'Miller';
+// john['job'] = 'programmer';
+//
+// console.log(john);
+//
+//
+// let jane = new Object();
+// jane.name = 'Jane';
+// jane.lastName = 'Smith';
+// jane['yearOfBirth'] = 1969;
+// jane['job'] = 'retired';
+// jane['isMarried'] = true;
+//
+// console.log(jane);
 
-let writeToVariable = box.material;
-
-box.material = "newValue";
-
-box["material"] = "secondValue";
-let func = function () {
-    return "material";
+let john = {
+    name: 'John',
+    lastName: 'Smith',
+    yearOfBirth: 1990,
+    job: 'teacher',
+    isMarried: false,
+    family: ['Jane', 'Mark', 'Bob'],
+    calculateAge: function() {
+        return 2016 - this.yearOfBirth;
+    }
 };
 
-box["size"] = {height : 20, width : 50};
+//console.log(john.calculateAge(1970));
+console.log(john.calculateAge());
 
-// area is a method
-box.area = function () {
-    return box.size.height * box.size.width;
-}
+let age = john.calculateAge();
+john.age = age;
 
-for(var key in box){
-    // no gerentee order
-    // console.log(key);
-    // console.log(box[key]);
-}
+console.log(john);
 
-let animal = [];
 
-animal.username = 'Mittens';
 
-animal['tagline'] = 'Pet me!';
-
-let noises = [];
-animal.noises = noises;
-
-let count = 0;
-for(let key in animal){
-    count++;
-    if(key === 'username'){
-        console.log('Hi my name is ' + animal[key]);
-    } else if(key === 'tagline'){
-        console.log('I like to say ' + animal[key]);
+//v2.0
+let john = {
+    name: 'John',
+    lastName: 'Smith',
+    yearOfBirth: 1990,
+    job: 'teacher',
+    isMarried: false,
+    family: ['Jane', 'Mark', 'Bob'],
+    calculateAge: function() {
+        this.age = 2016 - this.yearOfBirth;
     }
-}
+};
 
-console.log(animal);
+john.calculateAge();
+console.log(john);
 
 
+let mike = {
+    yearOfBirth: 1950,
+    calculateAge: function() {
+        this.age = 2016 - this.yearOfBirth;
+    }
+};
+
+mike.calculateAge();
+console.log(mike);
 
 
 
