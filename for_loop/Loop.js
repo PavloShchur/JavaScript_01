c34967ec03e681d80630d8a21e8aec66938fbbcd
Pavlo Shchur <pavloshchur@gmail.com>
2018-02-01 23:56:21 +0200

    JavaScript for loop.

---
+++ for_loop/Loop.js
@@ -0,0 +1,20 @@
+window.onload = function () {
+
+    let x = 50;
+    // while(x > 0){
+    //     document.getElementById('result').innerHTML += '<h2>' + x + '</h2>';
+    //     x--;
+    // }
+    //
+    // let input = '';
+    //
+    // while(input !== 'XXX'){
+    //     input = prompt('Enter your value.');
+    //     alert('continue');
+    // }
+
+    for (let y = 0; y < 500; y += 20) {
+        console.log(y);
+    }
+
+};
