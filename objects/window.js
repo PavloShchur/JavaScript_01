8f94ed9b226039f2f2143e1b43886f5196d4bfc5
Pavlo Shchur <pavloshchur@gmail.com>
2018-02-18 20:43:03 +0200

    JavaScript : window object.

---
+++ objects/window.js
@@ -0,0 +1,115 @@
+// WINDOW METHODS / OBJECTS / PROPERTIES
+
+// window.console.log('window');
+
+// alert
+
+// window.alert('Hello World!');
+
+// prompt
+
+// let input = window.prompt();
+// window.alert(input);
+
+// confirm
+
+// if(window.confirm('Are you sure?')){
+//     window.console.log('YES');
+// } else {
+//     window.console.log('NO');
+// }
+
+let val;
+
+// outer height and width
+// val = window.outerHeight;
+// val = window.outerWidth;
+
+// inner height and width
+// val = window.innerHeight;
+// val = window.innerWidth;
+
+// scroll points
+
+val = window.scrollY;
+val = window.scrollX;
+
+// location object
+
+val = window.location;
+val = window.location.hostname;
+val = window.location.port;
+val = window.location.href;
+val = window.location.search;
+
+// redirect
+// window.location.href = 'http://google.com';
+
+// reload
+// window.location.reload();
+
+// HISTORY OBJECT
+// window.history.go();
+val = window.history.length;
+
+// navigator object
+val = window.navigator;
+val = window.navigator.appName;
+val = window.navigator.appVersion;
+val = window.navigator.userAgent;
+val = window.navigator.platform;
+val = window.navigator.vendor;
+val = window.navigator.language;
+
+
+window.console.log(val);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
