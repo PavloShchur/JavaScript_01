c7763d9dee35d92e6ab436c70ac75572d1916fd5
Pavlo Shchur <pavloshchur@gmail.com>
2018-03-05 22:54:24 +0200

    JavaScript : function.js is modified.

--- function/function.js
+++ function/function.js
@@ -47,6 +47,52 @@ let arrowSum2 = (a, b) => {
 };
 console.log('Arrow function ' + arrowSum(5, 75));
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function plus(a , b) {
+    return (
+        console.log('plus'),
+        console.log(a + b),
+        console.log(this),
+        console.log(arguments)
+    )
+}
+
+plus(2,2);
+
+// using functions as methods
+console.log('calc');
+
+let calc = {
+    status: 'Awesome',
+    plus: function (a,b) {
+        return (
+            console.log(this),
+            console.log(a+b),
+            console.log(arguments),
+            console.log(this.status)
+        )
+    }
+};
+
+calc.plus(2,2);
+
+// invoking instances through the constructor
+console.log('dir');
+let Dog = function() {
+    let name, breed;
+    return console.dir(this);
+};
+
+firstDog = new Dog;
+firstDog.name = "Rover";
+firstDog.breed = "Doberman";
+
+secondDog = new Dog;
+secondDog.name = "Fluffy";
+secondDog.breed = "Poodle";
+
+
 
 
 
